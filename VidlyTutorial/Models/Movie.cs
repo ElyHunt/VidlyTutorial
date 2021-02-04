15ccbaf580255ced7c43449d7d8da270407d516c
Ellie <elykuu@gmail.com>
2021-02-04 00:32:53 -0500

    Created Movie class

---
+++ VidlyTutorial/Models/Movie.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace VidlyTutorial.Models
+{
+    public class Movie
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+    }
+
+    // /movies/random
+    // We want to create a page called movies controller with an action called random.
+}
